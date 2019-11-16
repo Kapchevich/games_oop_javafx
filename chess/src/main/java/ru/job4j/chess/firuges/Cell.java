901637161efe1200152a9dbdb500232e91b013bd
Kapchevich <kapchevich@gmail.com>
2019-11-16 19:40:11 +0300

    Каркас шахматной доски[#180744]

--- chess/src/main/java/ru/job4j/chess/firuges/Cell.java
+++ chess/src/main/java/ru/job4j/chess/firuges/Cell.java
@@ -13,8 +13,17 @@ public enum Cell {
     public final int x;
     public final int y;
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     Cell(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
 }
--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,5 +1,6 @@
 package ru.job4j.chess.firuges.black;
 
+import ru.job4j.chess.Logic;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
@@ -22,26 +23,35 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
+      int size = Math.abs(source.x - dest.x);
+        Cell[] steps = new Cell[size];
+        int x = source.getX();
+        int y = source.getY();
+        try {
+            for (int index = 0; index < size; index++) {
+                int deltaX = source.x < dest.x ? x++ : x--;
+                int deltaY = source.y < dest.y ? y++ : y--;
+                if (source.x == dest.x) {
+                    x = source.x;
+                }
+                if (source.y == dest.y) {
+                    y = source.y;
+                }
+                steps[index] = Cell.values()[x * 8 + y];
+                if (source.x == x) {
+                    throw new IllegalStateException();
+                } else if (source.y == y) {
+                    throw new IllegalStateException();
+                }
+            }
+        } catch (IllegalStateException e) {
+            System.out.println(String.format("Could not move by diagonal from %s to %s", source, dest));
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
+
         return false;
     }
 
